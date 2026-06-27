dd71d848018b4ab90e4d336b9898140516e90bcb
Pedro Silva <pedrinhopedrao135@gmail.com>
2026-06-27 19:44:49 -0300

    Implementação do dashboard administrativo com estatísticas

--- ProjetoBiblioteca/Controllers/AdminController.cs
+++ ProjetoBiblioteca/Controllers/AdminController.cs
@@ -1,11 +1,24 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ProjetoBiblioteca.Data;
 
 namespace ProjetoBiblioteca.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.TotalLivros = _context.Livros.Count();
+            ViewBag.TotalServicos = _context.Servicos.Count();
+
             return View();
         }
     }
