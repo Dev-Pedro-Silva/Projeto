fe0a72050190e27e8dff15c39aa0e1531fc7add1
Pedro Silva <pedrinhopedrao135@gmail.com>
2026-06-28 13:14:31 -0300

    Refinamento da tela Home, com apresentação e destaque para últimos livros

--- ProjetoBiblioteca/obj/Debug/net10.0/ProjetoBiblioteca.AssemblyInfo.cs
+++ ProjetoBiblioteca/obj/Debug/net10.0/ProjetoBiblioteca.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoBiblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+821603fc9921bf4f6f3b8bcb401c68eb610b17a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+969df6e950480f14ac87303e8b5fdeae38c64ece")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoBiblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoBiblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
